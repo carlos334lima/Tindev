96de61e29dd1e062ef232bd7f4ecb4da4e4e85b3
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2021-04-27 20:56:44 -0300

    Add dependency

---
+++ web/src/pages/Login/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import api from "../../services/api";
+
+import Logo from "../../assets/logo.svg";
+
+import "./index.css";
+
+const Login = () => {}
+
+export default Login;
---
+++ web/src/pages/Main/index.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import io from "socket.io-client";
+import api from "../../services/api";
+import { randomList } from "../../services/helpers";
+
+import Logo from "../../assets/logo.svg";
+import Like from "../../assets/like.svg";
+import Dislike from "../../assets/dislike.svg";
+import ItsAMatchLogo from "../../assets/itsamatch.png";
+
+import "./index.css";
+
+const Main = () => {}
+
+export default Main
