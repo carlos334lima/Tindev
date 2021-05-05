96a358b21c75710ab4bc191f934a9b79472eee98
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2021-05-05 15:55:04 -0300

    Front-end Web finished

--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -3,8 +3,42 @@ import api from "../../services/api";
 
 import Logo from "../../assets/logo.svg";
 
-import "./index.css";
+import "./styles.css";
 
-const Login = () => {}
+const Login = ({ history }) => {
+  //Recuperar o ID assim que que APi retornar, depois passar via parametro para a proxima tela= usar history
+
+  const [username, setUsername] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post("/devs", {
+      username,
+    });
+
+    const { _id } = response.data;
+
+
+    history.push(`/dev/${_id}`)
+  }
+
+  return (
+    <div className="login-container">
+      <form>
+        <img src={Logo} alt="Logo" />
+
+        <input
+          placeholder="Digite Seu Usuário do Github"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
--- web/src/pages/Main/index.js
+++ web/src/pages/Main/index.js
@@ -1,16 +1,84 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import io from "socket.io-client";
+/* import io from "socket.io-client"; */
 import api from "../../services/api";
-import { randomList } from "../../services/helpers";
+/* import { randomList } from "../../services/helpers"; */
 
 import Logo from "../../assets/logo.svg";
 import Like from "../../assets/like.svg";
 import Dislike from "../../assets/dislike.svg";
 import ItsAMatchLogo from "../../assets/itsamatch.png";
 
-import "./index.css";
+import "./styles.css";
 
-const Main = () => {}
+const Main = ({ match }) => {
+  const [users, setUsers] = useState([]);
 
-export default Main
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs", {
+        headers: {
+          user: match.params.id,
+        },
+      });
+
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [match.params.id]);
+
+  async function handleDislike(id) {
+    const response = await api.post(`/devs/${id}/likes`, null, {
+      headers: {
+        user: match.params.id,
+      },
+    });
+
+    setUsers(users.filter((user) => user._id !== id)); //Deu dislike? remove ele
+  }
+  async function handleLike(id) {
+    const response = await api.post(`/devs/${id}/likes`, null, {
+      headers: {
+        user: match.params.id,
+      },
+    });
+
+    setUsers(users.filter((user) => user._id !== id)); //Deu like? remove ele
+  }
+
+  return (
+    <div className="main-container">
+      <img src={Logo} alt="Logo" />
+
+      {users.length > 0 ? (
+        <ul>
+          {users.map((user) => (
+            <li key={user._id}>
+              <img src={user.avatar} alt="" />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+
+              <div className="buttons">
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={Like} alt="" />
+                </button>
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={Dislike} alt="" />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">
+          <h1>Por enquanto não Devs 😥</h1>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Main;
